18005eb50a4d7c89fd89e77e250a087d7e1d38e5
lion7472 <m.ghaffari662@gmail.com>
2021-07-27 20:42:13 +0430

    first commit from new laptop

---
+++ billing/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from core.models import Shipper, DesignAppendCategory, DesignAppend
+
+
+class ShipperSerializer(serializers.ModelSerializer):
+    """Serializer for shipper object"""
+
+    class Meta:
+        model = Shipper
+        fields = ('id', 'company_name')
+        read_only_fields = ('id',)
+
+
+class DesignAppendCategorySerializer(serializers.ModelSerializer):
+    """Seializer for OrderItemAppendCategory object"""
+
+    class Meta:
+        model = DesignAppendCategory
+        fields = ('id', 'type_name')
+        read_only_fields = ("id",)
+
+
+class DesignAppendSerializer(serializers.ModelSerializer):
+    """Serializer for OrderItemAppend objects"""
+
+    class Meta:
+        model = DesignAppend
+        fields = ('id', 'name', 'image', 'design_append_category',
+                  'design_append_price_irr')
+        read_only_fields = ('id',)
